9edcca6b296e1799690c58fb28240c1407b7bb2b
Nathx <nathankiner@gmail.com>
2022-01-29 20:35:52 +0100

    Added overlay toggle and legend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useRef, useEffect, useState } from 'react';
 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import Optionsfield from './components/Optionsfield';
+import Legend from './components/Legend';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibmF0azciLCJhIjoiY2t5dmc0anBpMHk4bzJybzhkdGg0MHdzdCJ9.nTfTTvKc1T0IQeIZocIOQA';
 
@@ -13,38 +15,109 @@ export default function App() {
 	const [lat, setLat] = useState(51.1657);
 	const [zoom, setZoom] = useState(5);
 
+	const options = [
+	    {
+	      name: 'Key Biodiversity Areas',
+	      description: 'Natura 2000 sites incl. 2 km buffer zones.',
+	      layer: 'n2k-germany',
+	      stops: [
+	        ["Cropland", '#ecf240'],
+	        ["Woodland and forest", '#3d840b'],
+	        ["Grassland", '#e4bb07'],
+	        ["Heathland and scrub", '#a4c73d'],
+	        ["Open spaces with little or no vegetation", '#52ffb4'],
+	        ["Wetland", '#b472ee'],
+	        ["Water", '#294ffa']
+	      ]
+	    },
+	    {
+	      name: 'Soil carbon',
+	      description: 'Organic carbon stored in soil (g C/kg), derived from LUCAS topsoil sample dataset.',
+	      layer: 'organic_carbon',
+	      stops: [
+	        [10.6, '#d7191c'],
+	        [21.2, '#fdae61'],
+	        [33.5, '#ffff'],
+	        [51.5, '#abdda4'],
+	        [905.2, '#2b83ba'],
+	      ]
+	    },
+	    {
+	      name: 'Forestry',
+	      description: 'Tree cover detected from satellite imagery.',
+	      layer: 'forest_types',
+	      stops: [
+	        ["Tree Cover", '#22751e']
+	      ]
+	    }
+    ];
+    const [active, setActive] = useState(options[0]);
+
 	useEffect(() => {
-	if (map.current) return; // initialize map only once
-	map.current = new mapboxgl.Map({
-	  container: mapContainer.current,
-	  style: 'mapbox://styles/natk7/ckyvg82kk003b14n1tuq11p21',
-	  center: [lng, lat],
-	  zoom: zoom
-	});
-	map.current.addControl(
-		new MapboxGeocoder({
-			accessToken: mapboxgl.accessToken,
-			mapboxgl: mapboxgl
+		if (map.current) return; 
+		// initialize map only once
+		map.current = new mapboxgl.Map({
+		  container: mapContainer.current,
+		  style: 'mapbox://styles/natk7/ckyvg82kk003b14n1tuq11p21',
+		  center: [lng, lat],
+		  zoom: zoom
+		});
+
+		// Adding search box for addresses.
+		map.current.addControl(
+			new MapboxGeocoder({
+				accessToken: mapboxgl.accessToken,
+				mapboxgl: mapboxgl
+			})
+		);
+
+		map.current.on('load', () => {
+			for (const option of options) {
+				if (active !== option) {
+					map.current.setLayoutProperty(option.layer, 'visibility', 'none');
+				}
+			};
 		})
-	);
 	});
 
 	useEffect(() => {
-	if (!map.current) return; // wait for map to initialize
-	map.current.on('move', () => {
-	  setLng(map.current.getCenter().lng.toFixed(4));
-	  setLat(map.current.getCenter().lat.toFixed(4));
-	  setZoom(map.current.getZoom().toFixed(2));
+		if (!map.current) return; // wait for map to initialize
+		map.current.on('move', () => {
+		  setLng(map.current.getCenter().lng.toFixed(4));
+		  setLat(map.current.getCenter().lat.toFixed(4));
+		  setZoom(map.current.getZoom().toFixed(2));
+		});
 	});
-	});
+
+	const changeState = i => {
+		setActive(options[i]);
+		for (let j = 0; j < options.length; j++) {
+			if (i == j) {
+				map.current.setLayoutProperty(options[j].layer, 'visibility', 'visible');
+			}
+			else {
+				map.current.setLayoutProperty(options[j].layer, 'visibility', 'none');
+
+			}
+		};
+    };
+
+// 	<!--- 
+// <div className="sidebar">
+//         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+//     </div> 
+// --->
 
   return (
     <div>
 
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
       <div ref={mapContainer} className="map-container" />
+      <Legend active={active} stops={active.stops} />
+      <Optionsfield
+        options={options}
+        layer={active.layer}
+        changeState={changeState}
+      />
     </div>
   );
 }
